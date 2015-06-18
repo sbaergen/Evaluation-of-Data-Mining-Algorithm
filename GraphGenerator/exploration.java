2398dba58de8215ef62d1bf5fc8114ae257fd6f6
sbaergen <sbaergen@ualberta.ca>
2015-06-18 16:02:51 -0600

    Ran small tests (Memory issues)

--- GraphGenerator/exploration.java
+++ GraphGenerator/exploration.java
@@ -4,15 +4,15 @@ import java.util.Random;
 public class exploration {
 
 	public static void main(String[] args) {
-        final int[] MAXATTR = { 1, 1000 };
-        final int[] MAXFORWARD = { 0, 100 };
-        final int[] MAXBACKARD = { 0, 100 };
-        final int[] GAP = { 0, 20 };
-        final double[] MINSUPPORT = { 0, 1 };
-        final int[] MAXNODES = { 10, 100 };
-        final int[] EFGS = { 1, 100 };
-        final int[] NODES = { 10, 10000 };
-        final int[] ATTR = { 10, 100 };
+        final int[] MAXATTR = { 1, 10 };
+        final int[] MAXFORWARD = { 0, 10 };
+        final int[] MAXBACKARD = { 0, 10 };
+        final int[] GAP = { 0, 10 };
+        final double[] MINSUPPORT = { 0, 0 };
+        final int[] MAXNODES = { 10, 50 };
+        final int[] EFGS = { 1, 10 };
+        final int[] NODES = { 10, 100 };
+        final int[] ATTR = { 10, 50 };
         final double[] APROB = { 0.1, 1 };
         final double[] EPROB = { 0, 1 };
         final char[] DIST = { 'U', 'E', 'G' };
